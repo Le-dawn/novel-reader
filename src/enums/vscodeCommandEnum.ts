3e12485762e34b664029bb16661fe8e0c24deaf6
lijinfeng <lijinfeng@yuxirj.com>
2025-09-29 10:35:09 +0800

    feat: 添加全局状态和VSCode命令枚举

---
+++ src/enums/vscodeCommandEnum.ts
@@ -0,0 +1,9 @@
+export enum VscodeCommandEnum {
+    IMPORT_NOVEL = 'novelReader.importNovel',
+    OPEN_CHAPTER = 'novelReader.openChapter',
+    SHOW_CURRENT_CHAPTER = 'novelReader.showCurrentChapter',
+    NEXT_CHAPTER = 'novelReader.nextChapter',
+    PREVIOUS_CHAPTER = 'novelReader.previousChapter',
+    REFRESH_SIDEBAR = 'novelReader.refreshSidebar',
+    DELETE_NOVEL = 'novelReader.deleteNovel'
+}
